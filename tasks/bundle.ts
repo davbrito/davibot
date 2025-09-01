d4253841bba0803acff486dae67416e743518cb5
David Brito <39559632+davbrito@users.noreply.github.com>
2025-09-01 12:04:10 -0400

    This reverts commit 5db8df72b5ba5a777b50c9460447180e1d359d59.

--- tasks/bundle.ts
+++ tasks/bundle.ts
@@ -1,7 +1,10 @@
-import { parseArgs } from "@std/cli/parse-args";
+import { denoPlugins } from "@luca/esbuild-deno-loader";
 import { fromFileUrl } from "@std/path";
+import * as esbuild from "esbuild";
+import denoConfig from "../deno.json" with { type: "json" };
 import { z } from "zod";
 import { env } from "../lib/env.ts";
+import { parseArgs } from "@std/cli/parse-args";
 
 const { MINIFY } = env({
   MINIFY: z.stringbool().default(true),
@@ -17,25 +20,28 @@ const { minify } = parseArgs(Deno.args, {
 const entry = import.meta.resolve("../src/main.tsx");
 const outdi = fromFileUrl(import.meta.resolve("../dist/"));
 
-const args = [
-  "bundle",
-  "-I",
-  "--outdir",
-  outdi,
-  "--format=esm",
-  "--config",
-  fromFileUrl(import.meta.resolve("../deno.json")),
-  "--sourcemap",
-  // "--code-splitting",
-  entry,
-];
+const result = await esbuild.build({
+  entryPoints: [entry],
+  format: "esm",
+  target: "deno2.4",
+  outdir: outdi,
+  plugins: [
+    ...(denoPlugins({
+      configPath: fromFileUrl(import.meta.resolve("../deno.json")),
+    }) as esbuild.Plugin[]),
+  ],
+  jsx: "automatic",
+  jsxImportSource: denoConfig.compilerOptions.jsxImportSource,
+  bundle: true,
+  minify: minify,
+  sourcemap: true,
+  treeShaking: true,
+  metafile: true,
+});
 
-if (minify) {
-  args.push("--minify");
-}
+await Deno.writeTextFile(
+  fromFileUrl(import.meta.resolve("../dist/metafile.json")),
+  JSON.stringify(result.metafile, null, 2),
+);
 
-await new Deno.Command(Deno.execPath(), {
-  args: args,
-})
-  .spawn()
-  .output();
+esbuild.stop();
