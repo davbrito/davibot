66b2d153a66f1eae38bc3d348c529c33d3178f92
David Brito <39559632+davbrito@users.noreply.github.com>
2025-09-01 15:06:19 -0400

    remove test

--- src/interfaces/jobs/controllers.ts
+++ src/interfaces/jobs/controllers.ts
@@ -2,4 +2,3 @@ import { dailyXkcdJobHandler } from "./handlers.ts";
 
 // daily at 9 AM Venezuela timezone (-4 UTC)
 Deno.cron("send daily xkcd", "0 13 * * *", dailyXkcdJobHandler); // Adjusted to UTC
-Deno.cron("send minutely xkcd", "* * * * *", dailyXkcdJobHandler);
